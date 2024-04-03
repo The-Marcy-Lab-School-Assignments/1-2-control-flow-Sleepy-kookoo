d68e38c27c3974d80ff7b63e9a0002357a18c296
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-03 12:51:45 -0400

    Answered Q1 + working on Q2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,33 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought')
+  } else if (inches < 2) {
+    console.log('dry')
+  } else if (inches < 4) {
+    console.log('average')
+  } else if (inches < 6) {
+    console.log('rainy')
+  } else if (inches >= 6) {
+    console.log('flood')
+  }
 };
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+  if (breed === 'snake') {
+    console.log('Hiss hiss!')
+  } else if (breed === 'cat' && age < 5) {
+    console.log('Mew mew!')
+  } else if (breed === 'cat' && age >= 5) {
+    console.log('Meow meow!')
+  } else if (breed === 'dog' && age < 5) {
+    console.log('Arf arf!')
+  } else if (breed === 'dog' && 5 < age < 10) {
+    console.log('Woof woof')
+  } else if (breed === 'dog' && age >= 10) {
+    console.log('Boof!')
+  } else {
+    console.log('Happy birthday!')
+  }
 };
 
 const funTypes = () => {

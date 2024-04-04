6c0d50b9cb7cb5408a2282d43211a1ae4e4b86df
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-04 12:39:52 -0400

    Answered Q6

--- src/modify.js
+++ src/modify.js
@@ -1,11 +1,12 @@
 const wildlyBiasedReview = (location) => {
+  if (typeof location !== 'string') return "Non-existing"
   if (location === 'NYC') {
     console.log('THE GREATEST CITY IN THE WORLD');
     console.log('THE CITY THAT NEVER SLEEPS');
     console.log('WOW WHAT A CITY');
   } else {
     console.log('Yea that place is cool I guess');
-  }
+  };
 };
 
 module.exports = {

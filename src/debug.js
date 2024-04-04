47ef4c33a32cae977b3d8634e78a33fc967c9956
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-04 13:45:50 -0400

    Answered Q11

--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,5 @@
 const getRandomIntInRange = (min, max) => {
-  if (min > max) throw new Error('min must be less than max');
+  if (min > max) throw new Error("min must be less than max");
   return Math.floor(Math.random() * max);
 };
 
@@ -37,16 +37,19 @@ const getWeatherReport = (temperature) => {
   return weatherReport + "And that's your report!"
 };
 
-console.log(getWeatherReport(92))
+//console.log(getWeatherReport(92))
 // instead of console.log(And thats your report) include it in the return function
 // We have to make sure it does not have const inside the if statements
 // we would just console.log the string no need for const .. 
 const returnPositiveNegativeZero = (num) => {
-  return num < 0
-    ? "Positive"
-    : num === 0
-      ? "Zero"
-      : "Negative";
+  if (num === 0) {
+    return "Zero"
+  }
+  return num < 0 ? "Negative" : "Positive"
+  //return num < 0 ? "Negative" : "Positive"
+  // : num === 0
+  //   ? "Zero"
+  //   : "Negative";
 };
 // current idea; it is trying to return 0 as positive and negative? 
 // num < 0 && num > 0
